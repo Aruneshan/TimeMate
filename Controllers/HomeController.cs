978635de4664e7084f331eb44b4d74a2529f9ec1
Aruneshan <aruneshan@gmail.com>
2023-12-14 01:34:59 +0530

    feedback

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -43,26 +43,27 @@ namespace TimeMate.Controllers
             return View();
         }
 
-        [HttpPost]
-        [Authorize]
-        [TypeFilter(typeof(CustomAuthenticationFilter))]
-
-        public async Task<IActionResult> SubmitFeedback(Feedback feedback)
+    [HttpPost]
+    [Authorize]
+    [TypeFilter(typeof(CustomAuthenticationFilter))]
+    public async Task<IActionResult> SubmitFeedback(Feedback feedback)
+    {
+        try
         {
-            if (feedback != null && ModelState.IsValid) 
+            if (feedback != null && ModelState.IsValid)
             {
-                var UserName = User.FindFirstValue(ClaimTypes.Name);
+                var userName = User.FindFirstValue(ClaimTypes.Name);
                 var email = User.FindFirstValue(ClaimTypes.Email);
-               
-                feedback.UserName = UserName;
+
+                feedback.UserName = userName;
                 feedback.Email = email;
 
                 var json = JsonConvert.SerializeObject(feedback);
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                using (var client = new HttpClient())
+                using (var client = _httpClientFactory.CreateClient())
                 {
-                    var apiUrl = "https://localhost:7051/api/FeedBack"; 
+                    var apiUrl = "http://localhost:5089/api/FeedBack"; // Replace with your API endpoint
                     var response = await client.PostAsync(apiUrl, content);
 
                     if (response.IsSuccessStatusCode)
@@ -70,13 +71,23 @@ namespace TimeMate.Controllers
                         // Feedback submitted successfully
                         return RedirectToAction("Index");
                     }
+                    else
+                    {
+                        // Log or handle the unsuccessful response
+                        _logger.LogError("Failed to submit feedback. Status code: {StatusCode}", response.StatusCode);
+                    }
                 }
             }
-
-            // Feedback Failed
-            return View("Error");
+        }
+        catch (HttpRequestException ex)
+        {
+            // Log or handle the exception
+            _logger.LogError(ex, "Error submitting feedback");
         }
 
+        // Handle any other errors or redirect to an error page
+        return View("Error");
+    }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
